9ad20950504723a57f9db5641ffe179d3653f12e
Wekn pvt <87528684+Wekn@users.noreply.github.com>
2021-07-17 18:17:36 +0530

    app.js changed

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 // in sublime
-var express = require('express');
+var express = require("express");
+const Api = require("./src/api/index");
 var port = process.env.PORT || 3000;
 var app = express();
-
-
-
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); //
 
-app.get('/', function (req, res) {
-    console.log(req);
-    res.json({
-        status: true,
-        message: "Success"
-
-    });
-    // res.send(
-
-    //     JSON.stringify({ Hello: 'World' }));
+app.use(express.json());
+app.use("/api", Api);
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to express app"
+  });
 });
-app.listen(port, function () {
-    console.log(`Example app listening on port !`);
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
 });
