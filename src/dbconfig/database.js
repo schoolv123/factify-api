d79dcf06501909def63deef8a63b0e21747c3387
Wekn pvt <87528684+Wekn@users.noreply.github.com>
2021-08-04 21:58:18 +0530

    Update database.js

--- src/dbconfig/database.js
+++ src/dbconfig/database.js
@@ -1,11 +1,27 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-  host: "sql209.epizy.com",
-  user: "epiz_29151775",
+  host: "remotemysql.com",
+  user: "oja97cBpLs",
   password: "bCeXVv7izz",
-  database: "epiz_29151775_factify",
+  database: "oja97cBpLs",
   connectionLimit: 10
 });
 
 module.exports = pool;
+
+
+
+
+
+// const mysql = require("mysql");
+
+// const pool = mysql.createPool({
+//   host: "sql209.epizy.com",
+//   user: "epiz_29151775",
+//   password: "bCeXVv7izz",
+//   database: "epiz_29151775_factify",
+//   connectionLimit: 10
+// });
+
+// module.exports = pool;
